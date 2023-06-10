8466f1e1b9b2ef7727be7d97b793f770c8d7db74
qnhn2 <nhatquan234@gmail.com>
2023-06-10 12:13:51 +0700

    build edit and delete

---
+++ src/components/EditTask.jsx
@@ -0,0 +1,91 @@
+import React, { useEffect, useState } from 'react'
+import "./Form.css"
+import { supabase } from '../client';
+
+function EditTask() {
+    const [projects, setProjects] = useState({})
+    useEffect(() => {
+        const fetchProjects = async () => {
+            const { data } = await supabase
+                .from('Project')
+                .select()
+
+            data.map((project) => {
+                setProjects((prev) => ({
+                    ...prev,
+                    [project.name]: project.id
+                }))
+            })
+        }
+
+        const fetchTask = async () => {
+
+        }
+        fetchProjects();
+    }, [])
+
+    const [task, setTask] = useState({
+        project_id: "",
+        assignee: "",
+        due: "2022-12-22",
+        content: "",
+    })
+
+
+
+    const createTask = async (e) => {
+        e.preventDefault()
+        const { data, error } = await supabase
+            .from('Task')
+            .insert({
+                project_id: task.project_id,
+                assignee: task.assignee,
+                due: task.due,
+                content: task.content
+            })
+        window.location = "/"
+    }
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setTask((prevTask) => ({
+            ...prevTask,
+            [name]: value
+        }))
+    }
+
+    return (
+        <div className='form'>
+            {projects && (
+                <>
+                    <h3 className='title'>New Task</h3>
+                    <form className="input_form" onSubmit={createTask}>
+                        <div className='input_field'>
+                            <label className='input_label' htmlFor="project_id">Select a project</label><br></br>
+                            <select value={task.project_id} name="project_id" id="project_id" onChange={handleChange}>
+                                <option key="" value="">Choose a project</option>
+                                {Object.keys(projects).map((projectName) => (<option key={projectName} value={projects[projectName]}>{projectName}</option>))}
+                            </select>
+                        </div>
+                        <br></br>
+                        <div className='input_field'>
+                            <label className='input_label' htmlFor="assignee">Assignee</label><br></br>
+                            <input type='text' id='assignee' name='assignee' value={task.assignee} onChange={handleChange} />
+                        </div>
+                        <div className='input_field'>
+                            <label className='input_label' htmlFor="due">Due</label><br></br>
+                            <input type='date' id='due' name='due' value={task.due} onChange={handleChange} />
+                        </div>
+                        <div className='input_field'>
+                            <label className='input_label' htmlFor="content">Content</label><br></br>
+                            <textarea id='content' name='content' value={task.content} onChange={handleChange} />
+                        </div>
+                        <input type='submit' value={"New Task"} />
+                    </form>
+                </>
+            )}
+        </div>
+    )
+}
+
+export default EditTask
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,7 +1,33 @@
 import React from 'react'
 import "./Task.css"
+import Modal from 'react-bootstrap/Modal';
+import { useState } from 'react';
+import { supabase } from '../client';
 
-function Task({ project, assignee, due, content }) {
+function Task({ id, project, assignee, due, content }) {
+    const [show, setShow] = useState(false)
+
+    const handleClick = () => {
+        setShow(true)
+    }
+
+    const handleClose = () => {
+        setShow(false)
+    }
+
+    const handleDelete = async () => {
+        const { error } = await supabase
+            .from('Task')
+            .delete()
+            .eq('id', id)
+        console.log(error)
+        window.location = "/"
+    }
+
+    const handleEdit = async () => {
+
+    }
+
     return (
         <div className='card'>
             <div className='task_info'>
@@ -10,6 +36,21 @@ function Task({ project, assignee, due, content }) {
                 <p>Due: {due}</p>
             </div>
             <p className='task_content'>Detail: {content}</p>
+            <div className='task_button'>
+                <button className="done_btn" onClick={handleClick}>Done</button>
+                <button className='edit_btn' onClick={handleEdit}>Edit</button>
+            </div>
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Body>
+                    <p>Done the task!!! Do you want to delete this card?</p>
+                    <p>Project: {project}</p>
+                    <p>Detail: {content}</p>
+                </Modal.Body>
+                <Modal.Footer>
+                    <button className='modal_close' onClick={handleClose}>Close</button>
+                    <button className='model_delete' onClick={handleDelete}>Delete</button>
+                </Modal.Footer>
+            </Modal>
         </div>
     )
 }
